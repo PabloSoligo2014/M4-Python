14ca8f8d5c99bdd66c0dfa2e7fef6fe0d594b0b1
PabloSoligo2014 <pabli@NB-PABLO>
2019-08-09 10:24:20 -0300

    IO-Consola

---
+++ M4-Python/Main.py
@@ -0,0 +1,23 @@
+#coding=utf8
+#->La linea anterior habilita al uso de caracteres no ascii en los mensajes
+'''
+Created on 9 ago. 2019
+
+@author: pabli
+'''
+
+
+if __name__ == '__main__':
+    #->Ingreso simple por consola (Lo vamos a usar muy poco)
+    
+    x = input("Ingrese a numero:");
+    #->Punto y coma fin de sentencia, aunque la nueva linea ofrece lo mismo comun en python
+    
+    
+    #->Salida por consola simple y parametrizada.
+    print("El número ingresado es ", x, ".")
+    print("El numero ingresado es %d."%(int(x)))
+                                   
+                
+    
+    
