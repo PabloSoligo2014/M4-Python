a00736636e680642cd9d9a85aadc57dcce3405f1
PabloSoligo2014 <pabli@NB-PABLO>
2019-08-09 10:59:42 -0300

    0

--- M4-Python/Main.py
+++ M4-Python/Main.py
@@ -8,36 +8,7 @@ Created on 9 ago. 2019
 
 
 if __name__ == '__main__':
-    
-    #->Asignacion con = 
-    midpoint = 5
-    
-    #->Instancio dos listas vacias ==> lower = list()
-    #->Con ; salto la necesidad de nueva linea para terminar sentencia (No recomendado)
-    lower = []; upper = [];
-    
-    #->separo los numeros en "altos" y "bajos"
-    #->Iteracion definida, observar que la tabulacion define el bloque!!
-    for i in range(10):
-        #->Condicional
-        if (i<midpoint):
-            #->Agrego a la lista
-            lower.append(i)
-        else:
-            #->Agrego a la lista
-            upper.append(i)
-            
-    print("Lower:", lower)
-    print("Upper:", upper)
-    
-    
-    #->Iteraciones sobre listas, se crea la lista utilizando nombre clase
-    word_list = list()
-    
-    word_list.extend(["Hola", "Mundo", "!"])
-    
-    for word in word_list:
-        print(word)
+    pass
         
     
     
