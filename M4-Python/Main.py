e2fddc0e732a563b5b8f4e286d62f6928d13eccc
PabloSoligo2014 <pabli@NB-PABLO>
2019-08-14 10:24:27 -0300

    Parametros por posicion y por nombre

--- M4-Python/Main.py
+++ M4-Python/Main.py
@@ -9,8 +9,24 @@ import struct
 #import random
 
 
+def max(x, y):
+    if x>y:
+        return x
+    else:
+        return y
+   
+    
+def show_data(name, age, address):
+    print("Name: ", name, ",age:", age, ",address:", address)
+
 if __name__ == '__main__':
-    pass
+    
+    lx = 10
+    ly = 20
+    
+    print("El maximo entre lx y ly es:", max(lx, ly))    
+    show_data(address="Segurola y Havanna", name="Diego Armando Maradona", age=62)
+    
     
                            
     
